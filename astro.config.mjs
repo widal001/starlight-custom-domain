c5a1151eeead965cdb614f6e76660d35c54930b0
widal001 <billy.daly@agile6.com>
2025-03-13 09:54:05 -0400

    refactor: Configure for GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://widal001.github.io",
+  // Base needs trailing slash to make relative links work when hosting locally
+  base: "starlight-custom-domain/",
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/widal001/starlight-custom-domain",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
